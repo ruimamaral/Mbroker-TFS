0786e3a4110b3231f1638f7dba0fe08164b4f940
PF2100 <pedromfreitas@tecnico.ulisboa.pt>
2022-12-14 08:47:58 +0000

    mudança no unlink, diminuição do número de hardlinks antes do if

--- fs/operations.c
+++ fs/operations.c
@@ -269,10 +269,10 @@ int tfs_unlink(char const *target) {
 		fprintf(stderr, "directory lookup error: %s\n", strerror(errno));
       	return ERROR_VALUE;
    	}
-	
+
 	target_inode = inode_get(target_inumber);
 	type = target_inode->i_node_type;
-	int hardlinks = target_inode->hard_links--;
+	int hardlinks = --target_inode->hard_links;
 
 	if(type == T_SYMLINK){
 		inode_delete(target_inumber);
