8c234ffcffcae4343f4fec2b2427a53e2da677fd
PF2100 <pedromfreitas@tecnico.ulisboa.pt>
2023-01-10 22:22:58 +0000

    Makefile added, publisher and mbroker started

--- p2/mbroker/mbroker.c
+++ p2/mbroker/mbroker.c
@@ -1,9 +1,52 @@
 #include "logging.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include "betterassert.h"
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: mbroker <pipename>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+	int i;
+    if( argc == 2 ) {
+		return -1;
+	}
+	ALWAYS_ASSERT((int)argv[2] > 0,"Invalid session number\n");
+	pthread_t worker_threads[(int)argv[2]];
+	unlink(argv[1]);
+
+	if( mkfifo( argv[1],0777 ) == -1 ){
+		return -1;
+	}
+
+	/* for( i = 0; i < argv[2] ; i++) {
+		pthread_create(&worker_threads[0],NULL,&process_session,NULL);
+	} */
+
+	init_queue();
+	accept_register(argv[1]);
+}
+
+
+accept_register(char* pipe_name) {
+	int dummy_pipe;
+	int server_pipe;
+	ssize_t rest_of_message= 0;
+	if((server_pipe = open(pipe_name, O_RDONLY)) == -1 ){
+		return -1;
+	}
+	if((dummy_pipe = open(pipe_name, O_WRONLY)) == -1 ){
+			return -1;
+	}
+	while(true) {
+		ssize_t ret = read(server_pipe, ,);
+		switch(code) {
+			case:>"1" 
+
+		}
+	}
 }
--- p2/publisher/pub.c
+++ p2/publisher/pub.c
@@ -1,9 +1,92 @@
 #include "logging.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+
+
+
+void requestcpy(void* request, size_t* request_offset, void* data, size_t size) {
+    memcpy(request + *request_offset, data, size);
+    *request_offset += size;
+}
+
+int send_request(char** argv) {
+	int pd;
+	u_int8_t code = 1;
+	size_t request_offset = 0;
+	char request[289];
+	if((pd = open(argv[1], O_WRONLY)) == -1 ){
+		return -1;
+	}
+	memset(request, 0, sizeof(request));
+	requestcpy(request, &request_offset, &code, sizeof(u_int8_t));
+	requestcpy(request, &request_offset, argv[1], sizeof(char)*256);
+	requestcpy(request, &request_offset, argv[2], sizeof(char)*32);
+
+	ssize_t ret = write(pd, request , sizeof(request));
+	if (ret < 0) {
+		return -1;
+	}
+	close(pd);
+	return 0;
+}
+
+int send_msg(char* client_pipe) {
+	int cp;
+	int character;
+	ssize_t ret;
+	int i = 0;
+	if((cp = open(client_pipe, O_WRONLY)) == -1 ){
+		return -1;
+	}
+
+	char message[1024];
+	while( ((character = getchar()) != EOF)) {
+		if( character == '\n' ) {
+			i = 0;
+			if ((ret = write(cp,message, sizeof(message))) < 0) {
+				return -1;
+			}
+			memset(message,0,sizeof(message));
+		}
+		else{
+			message[i] = (char)character;
+		}
+		i++;
+	}
+	close(cp);
+	return 0; 
+}
+
+
 
 int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: pub <register_pipe_name> <box_name>\n");
-    WARN("unimplemented"); // TODO: implement
-    return -1;
+    if(argc != 4) {
+		printf("sai1\n");
+		return -1;
+	}
+
+	unlink(argv[2]);
+
+	if (mkfifo(argv[2],0777) == -1 ){
+		printf("sai3\n");
+		return -1;
+	}
+
+	if(send_request(argv) != 0 ) {
+		printf("sai4\n");
+		return -1;
+	}
+	if(send_msg(argv[2]) != 0 ){
+		printf("sai5\n");
+		return -1;
+	}
+
+    return 0;
 }
+
